039e0dec6333aec0f99a0cdeedce810618aeeabc
ohr486 <ohr486@gmail.com>
2023-03-19 10:01:53 +0900

    add comments

--- lib/n_plus_one/n1_check/detector.ex
+++ lib/n_plus_one/n1_check/detector.ex
@@ -11,18 +11,20 @@ defmodule NPlusOne.N1Check.Detector do
   end
 
   @impl true
+  # 引数のクエリが直前のクエリと同じ
   def handle_call({:check, query}, _from, %{query: query, counter: counter} = state) do
     {:reply, {:match, query, counter + 1}, Map.put(state, :counter, counter + 1)}
   end
 
   @impl true
-  def handle_call({:check, query}, _from, %{query: previous_query, counter: previous_count}) do
-    {:reply, {:no_match, previous_query, previous_count}, %{query: query, counter: 1}}
+  # 引数のクエリが直前のクエリと異なる
+  def handle_call({:check, query}, _from, %{query: prev_query, counter: prev_count}) do
+    {:reply, {:no_match, prev_query, prev_count}, %{query: query, counter: 1}}
   end
 
   @impl true
-  def handle_call(:reset, _from, %{query: previous_query, counter: previous_count}) do
-    {:reply, {:no_match, previous_query, previous_count}, %{query: nil, counter: 0}}
+  def handle_call(:reset, _from, %{query: prev_query, counter: prev_count}) do
+    {:reply, {:no_match, prev_query, prev_count}, %{query: nil, counter: 0}}
   end
 
 
@@ -38,11 +40,12 @@ defmodule NPlusOne.N1Check.Detector do
     case check(query) do
       {:match, query, count} ->
         :nothing
-      {:no_match, previous_query, count} ->
-        if count > 2 do
+      {:no_match, prev_query, prev_count} ->
+        # 同じクエリが複数回実行されている
+        if prev_count > 1 do
           IO.puts "==========> n+1 SQL query detected!"
-          IO.puts "            query: #{previous_query}"
-          IO.puts "            called: #{count} times"
+          IO.puts "            query: #{prev_query}"
+          IO.puts "            called: #{prev_count} times"
         end
     end
   end
--- lib/n_plus_one/n1_check/handler.ex
+++ lib/n_plus_one/n1_check/handler.ex
@@ -1,12 +1,14 @@
 defmodule NPlusOne.N1Check.Handler do
+  require Logger
+
   def handle_event([:n_plus_one, _, :query], _measurements, meta, _config) do
     #Logger.info("meta: #{inspect meta}")
     query = Map.get(meta, :query)
-    #Logger.info("query: #{inspect query}")
+    #Logger.info(">>>>>>>>>> query: #{inspect query}")
 
     #{:ok, result} = Map.get(meta, :result)
     #rows = Map.get(result, :num_rows)
-    #Logger.info("rows: #{inspect rows}")
+    #Logger.info(">>>>>>>>>> result-rows: #{inspect rows}")
 
     NPlusOne.N1Check.Detector.analyze(query)
   end
